c6bb20881efaca1c5674c29f979d69c78931a3fa
Duong Yen-Ly <yen-ly.duong@epitech.eu>
2021-04-28 15:06:27 +0200

    checking if Frameworks folder exists and create if doesn't

--- CreateFramework/CreateFramework/main.swift
+++ CreateFramework/CreateFramework/main.swift
@@ -8,17 +8,44 @@
 import Foundation
 import ArgumentParser
 
-struct Repeat: ParsableCommand {
-    @Option(name: .shortAndLong, help: "The number of times to repeat 'phrase'.")
-    var count: Int?
+struct CreateFrameworks: ParsableCommand {
+    @Option(name: .shortAndLong, help: "The path where the framework should be generated in.")
+    var path: String?
 
-    @Argument(help: "The phrase to repeat.")
-    var phrase: String
+    @Argument(help: "The name of the framework.")
+    var name: String
 
-    mutating func run() throws {
-        print(phrase)
+    func run() throws {
+        let defaultPath = path ?? "../Frameworks/"
 
+        let fm = FileManager.default
+//        var sourceFilesPath = "../Flag"
+//        let items = try fm.contentsOfDirectory(atPath: sourceFilesPath)
+//
+//        for item in items {
+//            print ("CONTENT = " + item)
+//        }
+//
+        let fileManager = FileManager.default
+        var isDir : ObjCBool = false
+        if fileManager.fileExists(atPath: "../Framework", isDirectory:&isDir) {
+            if isDir.boolValue {
+                print("directory")
+                // file exists and is a directory
+            } else {
+                print ("not directory")
+                // file exists and is not a directory
+            }
+        } else {
+            do {
+                try FileManager.default.createDirectory(atPath: defaultPath, withIntermediateDirectories: true, attributes: nil)
+            } catch {
+                print(error.localizedDescription)
+            }
+
+            // file does not exist
+        }
     }
 }
 
-Repeat.main()
+CreateFrameworks.main()
