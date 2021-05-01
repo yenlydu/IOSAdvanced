9a1839dd760f47c8e177331ef2c76f608a87552c
Duong Yen-Ly <yen-ly.duong@epitech.eu>
2021-05-01 17:00:23 +0200

    resolved files missing

--- Script/Frameworks/Home/Home/Classes/HomeBuilder.swift
+++ Script/Frameworks/Home/Home/Classes/HomeBuilder.swift
@@ -3,12 +3,12 @@ import UIKit
 class HomeBuilder {
 
     func build() -> UIViewController {
-        let router = Router()
+//        let router = Router()
         let presenter = HomePresenter()
 
         let viewController = UIStoryboard(name: "Home", bundle: Bundle(for: Home.self)).instantiateViewController(identifier: "Home") as! Home
         viewController.presenter = presenter
-        viewController.router = router
+//        viewController.router = router
         return viewController
     }
 }
